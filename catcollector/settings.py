e1717781bced0870a0cec3c705ea21ec256610ca
Elizabeth Parnell <elizabethparnell@Elizabeths-Mac-mini.local>
2023-10-09 09:49:39 -0400

    added the allowed host

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECERT_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://test-django-deploy.onrender.com']
 
 
 # Application definition
